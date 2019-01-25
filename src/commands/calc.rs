d82f0d670f47feeacc51cb3ffd4b7bbbafd2ccb8
Landon Mote <harmonepsilon@gmail.com>
2019-01-24 20:27:07 -0800

    Added beginnings of Currency Conversion

--- src/commands/calc.rs
+++ src/commands/calc.rs
@@ -1,7 +1,27 @@
+use reqwest::Client;
+
+#[derive(Deserialize, Debug)]
+struct Currency {
+    from: String,
+    to: String,
+    result: f64,
+}
+
 command!(convert(_ctx, msg, args) {
     let value = args.single::<f64>().unwrap();
     let curr_one = args.single::<String>().unwrap();
     let curr_two = args.single::<String>().unwrap();
 
+    let request_url = format!("http://data.fixer.io/api/convert
+                              ?access_key={}
+                              &from={}
+                              &to={}
+                              &amount={}",
+                              "49c3695a05794ed92cadf05df6b4d9cc", curr_one, curr_two, value);
 
+    let mut response = reqwest::get(&request_url)?;
+    let ans: Currency = response.json()?;
+    let answer = format!("{}", ans.result);
+
+    let _ = msg.reply(&answer);
 });
